c8d5f68a5bc7d83cd22d9787173922a7007238d9
Voytech97 <janosanonimowos@gmail.com>
2020-11-04 12:52:24 +0100

    "Cannot set property 'innerHTML' of null" fixed

--- content.js
+++ content.js
@@ -6,9 +6,9 @@ function getCases() {
     fetch('https://disease.sh/v3/covid-19/countries/poland?strict=true')
     .then(res => res.json())
     .then(data => {
-        cases.innerHTML = `Zarażonych: ${data.cases}(${data.todayCases})`;
-        recovered.innerHTML = `Wyzdrowiało: ${data.recovered}(${data.todayRecovered})`;
-        deaths.innerHTML = `Zmarło: ${data.deaths}(${data.todayDeaths})`;
+        if(cases) {cases.innerHTML = `Zarażonych: ${data.cases}(${data.todayCases})`;}
+        if(recovered){recovered.innerHTML = `Wyzdrowiało: ${data.recovered}(${data.todayRecovered})`;}
+        if(deaths){deaths.innerHTML = `Zmarło: ${data.deaths}(${data.todayDeaths})`;}
     })
 }
 
